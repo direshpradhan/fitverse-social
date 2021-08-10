7d3236210342c6b79bd2a8f5768332fc7919d0ec
Diresh Kumar <41542536+diresh18@users.noreply.github.com>
2021-08-10 23:24:43 +0530

    fix: login and signup page width issues

--- src/features/authentication/Login.jsx
+++ src/features/authentication/Login.jsx
@@ -23,8 +23,10 @@ export const Login = () => {
   }, [token, navigate]);
 
   return (
-    <div className="flex flex-col w-1/3 mt-52 mx-auto">
-      <h2 className="font-bold text-2xl">Login</h2>
+    <div className="flex flex-col w-11/12 md:w-1/2 lg:w-1/3 pt-52 mx-auto h-screen">
+      <h2 className="font-bold text-3xl text-center mb-4">
+        Login to Fitverse Social
+      </h2>
       <form className="flex flex-col" onSubmit={(event) => loginHandler(event)}>
         <input
           type="text"
@@ -35,7 +37,7 @@ export const Login = () => {
               ? setEmail(event.target.value)
               : setUsername(event.target.value)
           }
-          className="mt-4 py-2 px-4 border rounded"
+          className="mt-4 py-2 px-4 border rounded-md"
         />
         <input
           type="password"
@@ -43,9 +45,12 @@ export const Login = () => {
           name="password"
           value={password}
           onChange={(event) => setPassword(event.target.value)}
-          className="mt-4 py-2 px-4 border rounded "
+          className="mt-4 py-2 px-4 border rounded-md "
         />
-        <button type="submit" className="mt-6 bg-black text-white py-2 rounded">
+        <button
+          type="submit"
+          className="mt-6 bg-black text-white py-2 rounded-md"
+        >
           Login
         </button>
       </form>
--- src/features/authentication/Signup.jsx
+++ src/features/authentication/Signup.jsx
@@ -23,8 +23,10 @@ export const Signup = () => {
   }, [token, navigate]);
 
   return (
-    <div className="flex flex-col w-1/3 mt-40 mx-auto">
-      <h2 className="font-bold text-2xl">Signup</h2>
+    <div className="flex flex-col w-11/12 md:w-1/2 lg:w-1/3 pt-40 mx-auto">
+      <h2 className="font-bold text-3xl text-center mb-4">
+        Sign up for Fitverse Social
+      </h2>
       <form
         onSubmit={(event) => signupHandler(event)}
         className="flex flex-col"
@@ -34,34 +36,37 @@ export const Signup = () => {
             type="text"
             placeholder="First Name"
             onChange={(event) => setFirstName(event.target.value)}
-            className="mt-4 py-2 px-4 border rounded w-56"
+            className="mt-4 py-2 pl-4 w-1/2 mr-1 border rounded-md"
           />
           <input
             type="text"
             placeholder="Last Name"
             onChange={(event) => setLastName(event.target.value)}
-            className="mt-4 py-2 px-4 border rounded w-56"
+            className="mt-4 py-2 pl-4 w-1/2 ml-1 border rounded-md"
           />
         </div>
         <input
           type="text"
           placeholder="Username"
           onChange={(event) => setUsername(event.target.value)}
-          className="mt-4 py-2 px-4 border rounded"
+          className="mt-4 py-2 px-4 border rounded-md"
         />
         <input
           type="email"
           placeholder="Email"
           onChange={(event) => setEmail(event.target.value)}
-          className="mt-4 py-2 px-4 border rounded"
+          className="mt-4 py-2 px-4 border rounded-md"
         />
         <input
           type="password"
           placeholder="Password"
           onChange={(event) => setPassword(event.target.value)}
-          className="mt-4 py-2 px-4 border rounded"
+          className="mt-4 py-2 px-4 border rounded-md"
         />
-        <button type="submit" className="mt-6 bg-black text-white py-2 rounded">
+        <button
+          type="submit"
+          className="mt-6 bg-black text-white py-2 rounded-md"
+        >
           Signup
         </button>
       </form>
